6dc9c173f7688b0a9c89340f89c33b745bf0ac4d
Mariam Tarek <mariamtarek7144@gmail.com>
2025-11-28 20:58:10 +0200

    fixed structure of folders/backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-//main
+const express = require("express");
+const mongoose = require("mongoose");
+const User=require("./models/User");
+
+
+const app=express();
+
+mongoose.connect("mongodb://localhost:27017/redditClone")
+
+app.listen(3000, () => console.log("Server running on port 3000"));
